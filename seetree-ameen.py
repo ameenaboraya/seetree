bd6ebcbe09ee6cefd2a4bdec1b59d7f95faaf417
ameenaboraya <ameen1132@gmail.com>
2021-05-25 23:48:21 +0300

    comm2

--- seetree-ameen.py
+++ seetree-ameen.py
@@ -5,13 +5,30 @@ app = Flask (__name__)
 @app.route("/")
 
 def homepage():
-    return render_template("homepage.html", pagetitle="Homepage")#render m7zer lno html mbkom string..#pagetitle 3shan albase
+    return render_template("homepage.html", pagetitle=" Ameen Flask")#render m7zer lno html mbkom string..
+
+
+@app.route("/page404")
+
+def page404():
+    return render_template("page404.html",pagetitle="404")#render m7zer lno html mbkom string..
+
+
+@app.route("/page404image")
+
+def page404image():
+    return render_template("page404image.html",pagetitle="404")#render m7zer lno html mbkom string..
 
 
 @app.route("/about")
 
 def about():
-    return render_template("about.html",pagetitle="About")#render m7zer lno html mbkom string..
+    return render_template("about.html",pagetitle="about")#render m7zer lno html mbkom string..
+
+
+
+
+
 
 
 if __name__=="__main__":
