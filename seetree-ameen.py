5b0c601030d8757b18c08e31b611f97ed81f05e1
ameenaboraya <82150100+ameenaboraya@users.noreply.github.com>
2021-05-26 14:33:56 +0300

    Add files via upload

--- seetree-ameen.py
+++ seetree-ameen.py
@@ -1,13 +1,15 @@
 from flask import Flask , render_template
 from PIL import Image , ImageStat
 import  urllib.request
+import numpy as np
+
 
 app = Flask (__name__)
 
 @app.route("/")
 
 def homepage():
-    return render_template("homepage.html", pagetitle=" Ameen Flask")#render m7zer lno html mbkom string..
+    return render_template("homepage.html")#render m7zer lno html mbkom string..
 
 
 
@@ -18,16 +20,7 @@ def starting_url():
 
 
 
-@app.route("/page404")
 
-def page404():
-    return render_template("page404.html",pagetitle="404")#render m7zer lno html mbkom string..
-
-
-@app.route("/page404image")
-
-def page404image():
-    return render_template("page404image.html",pagetitle="404")#render m7zer lno html mbkom string..
 
 
 
@@ -50,26 +43,24 @@ def starting_work(IMAGE_FILE_NAME,FUNC_NAME):
 
     stat = ImageStat.Stat(img)
     if(not (FUNC_NAME=='min' or FUNC_NAME=='max' or FUNC_NAME=='mean' or FUNC_NAME=='median')):
-        return render_template("page404.html"), 404
+        if FUNC_NAME[0] != 'p' or not FUNC_NAME[1:].isnumeric() or int(FUNC_NAME[1:]) < 0 or int(FUNC_NAME[1:]) > 100:
+            return render_template("page404.html"), 404
+
 
     if(FUNC_NAME=='min'):
-        return render_template("showresults.html", IMG_NAME=IMAGE_FILE_NAME, value=stat.extrema, text=' Min values for each band in the image is')
+        minimum= [x[0] for x in stat.extrema]
+        return render_template("showresults.html", IMG_NAME=IMAGE_FILE_NAME, value=minimum, text=' Min values for each band in the image is')
     if (FUNC_NAME == 'max'):
-        return render_template("showresults.html", IMG_NAME=IMAGE_FILE_NAME, value=stat.extrema, text=' Max values for each band in the image is')
+        maximum = [x[1] for x in stat.extrema]
+        return render_template("showresults.html", IMG_NAME=IMAGE_FILE_NAME, value=maximum, text=' Max values for each band in the image is')
     if (FUNC_NAME=='mean'):
         return render_template("showresults.html", IMG_NAME=IMAGE_FILE_NAME, value=stat.mean, text=' Mean values for each band in the image is')
     if (FUNC_NAME == 'median'):
         return render_template("showresults.html", IMG_NAME=IMAGE_FILE_NAME, value=stat.median, text=' median values for each band in the image is')
-
-
-
-
-
-
-
-
-
-
+    if FUNC_NAME[0] == 'p' and FUNC_NAME[1:].isnumeric() and int(FUNC_NAME[1:]) >= 0 and int(FUNC_NAME[1:]) <= 100:
+        pixels = list(img.getdata())
+        percentile= np.percentile(pixels, int(FUNC_NAME[1:]))
+        return render_template("showresults.html", IMG_NAME=IMAGE_FILE_NAME, value=percentile,text= FUNC_NAME+' value  in the image is')
 
 if __name__=="__main__":
 
